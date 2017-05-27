723c37861e767746c2ecd51a5b8be19ef6299244
Ayush Saluja <ayush571995@outlook.com>
2017-05-27 18:06:22 +0530

    image added

---
+++ image/image_read.py
@@ -0,0 +1,18 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+img=cv2.imread('path',0)
+#cv2.startWindowThread()
+'''cv2.namedWindow("preview",cv2.WINDOW_NORMAL)
+cv2.imshow("preview", img) & 0xFF
+
+k=cv2.waitKey()
+if k==27:
+    x=1
+elif k==ord('s'):
+    cv2.imwrite('/home/ayush/Desktop/myself_grayscale.png',img)
+    cv2.destroyAllWindows()
+'''
+plt.imshow(img,cmap='gray',interpolation='bicubic')
+plt.xticks([]),plt.yticks([])
+plt.show()
