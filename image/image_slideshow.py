40f13215afe3532c82bdcedc7822e9009d1b9145
Ayush Saluja <ayush571995@outlook.com>
2017-05-28 21:52:14 +0530

    modified

--- image/image_slideshow.py
+++ image/image_slideshow.py
@@ -1,23 +1,27 @@
-import os
-import cv2,time
-absolute_path_list=[]
-for root,dirs,files in os.walk("path",topdown=False):
-    for names in files:
-        if '.JPG' in names:
-            absolute_path_list.append(os.path.join(root,names))
-for index in range(len(absolute_path_list)-1):
-    img1=cv2.imread(absolute_path_list[index])
-    img2=cv2.imread(absolute_path_list[index+1])
-    cv2.namedWindow('slideshow',cv2.WINDOW_NORMAL)
-    alpha=1.0
-    beta=0.0
-    while beta<=1.0:
-        dst=cv2.addWeighted(img1,alpha,img2,beta,0)
-        alpha-=0.2
-        beta+=0.2
-        cv2.imshow('slideshow',dst)
-        k=cv2.waitKey(1) & 0xFF
-        if k==ord('q'):
-            cv2.destroyAllWindows()
-            exit(0)
-    time.sleep(1)
+import os,cv2,time
+class Slideshow:
+
+
+    def __init__(self,path,time=3):
+        self.absolute_path_list=[]
+        self.time=time
+        for root,dirs,files in os.walk(path,topdown=False):
+            for names in files:
+                if '.jpg' in names or '.JPG' in names or '.png' in names or '.PNG' in names :
+                    self.absolute_path_list.append(os.path.join(root,names))
+        self.length_list=len(self.absolute_path_list)
+
+    def start_slideshow(self):
+        for index in range(self.length_list):
+            img1=cv2.imread(self.absolute_path_list[index])
+            cv2.namedWindow('slideshow',cv2.WINDOW_NORMAL)
+            cv2.imshow('slideshow', img1)
+            k = cv2.waitKey(1) & 0xFF
+            if k == ord('q') or k==27:
+                cv2.destroyAllWindows()
+                exit(0)
+            time.sleep(self.time)
+
+
+ob=Slideshow('/home/ayush/Desktop/',3)
+ob.start_slideshow()
