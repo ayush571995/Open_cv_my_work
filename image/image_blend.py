277798a2ef2ac4ed486ca48dee5ab039cf545e14
Ayush Saluja <ayush571995@outlook.com>
2017-12-24 01:16:06 +0530

    Image and video deleted

--- image/image_blend.py
+++
@@ -1,9 +0,0 @@
-import cv2
-img1=cv2.imread('/home/ayush/Desktop/board_image.jpg')
-img2=cv2.imread('/home/ayush/Desktop/cards.jpg')
-dst=cv2.addWeighted(img1,0.9,img2,0.9,3)
-cv2.namedWindow('final',cv2.WINDOW_NORMAL)
-cv2.imshow('final',dst)
-k=cv2.waitKey(0) & 0xFF
-if k==ord('q'):
-    cv2.destroyAllWindows()
