fa3107e27ebeff68ed5d169a67923f783a16ac8d
Ayush Saluja <ayush571995@outlook.com>
2017-12-24 01:26:40 +0530

    Video added

---
+++ video/video_new.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2
+import time
+cap = cv2.VideoCapture(0)
+dim=(int(cap.get(3)),int(cap.get(4)))
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+out = cv2.VideoWriter('output.avi',fourcc,15.0,dim)
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret==True:
+        frame = cv2.flip(frame,1)
+        out.write(frame)
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+# Release everything if job is finished
+cap.release()
+out.release()
+cv2.destroyAllWindows()
